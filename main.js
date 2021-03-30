62d91c186a4142f345bedf39f0527caff46705c2
woojisung5893 <80203079+woojisung5893@users.noreply.github.com>
2021-03-30 20:43:32 +0900

    3월30일

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const display = document.getElementById("log");
+const stayMenu = document.querySelector('.stayMenu');
+const battleMenu = document.querySelector('.battleMenu');
+
+var battleMaster = false;
 
 var log = function(msg,color){
     var info_log = document.createElement("p");
@@ -7,4 +11,28 @@ var log = function(msg,color){
     display.prepend(info_log);
 }
 
+var command = {
+    battle:{
+        on:function(){
+            battleMenu.classList.add("on"); 
+        },
+        off:function(){
+            battleMenu.classList.remove("on");
+        }
+    },
+    stay:{
+        on:function(){
+            stayMenu.classList.add("on");
+        },
+        off:function(){
+            stayMenu.classList.remove("on");
+        }
+    }
+}
+
+var battleTick = function(){
+    if(battleMaster==false){
+
+    }
+}
 log("테스트","red");
